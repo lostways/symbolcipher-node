dd8c475565ff284c7c300fc245185f1dda807ce3
lostways <andrew@lostways.com>
2014-11-15 11:38:10 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Symbol Cipher
+============
+
+
+symbol cipher for discreet communication
+
+usage:
+
+`symbolCipher.encipher("occultusnuntius");`
+
+`symbolCipher.decipher("øçç¨¬†¨ß˜¨˜†ˆ¨ß");`
---
+++ symbolcipher.js
@@ -0,0 +1,25 @@
+//converts symbols to ascii
+var symbolCipher = {
+	conversion_table: {"A":"å","B":"∫","C":"ç","D":"∂","E":"´","F":"ƒ","G":"©","H":"˙","I":"ˆ","J":"∆","K":"˚","L":"¬","M":"µ","N":"˜","O":"ø","P":"π","Q":"œ","R":"®","S":"ß","T":"†","U":"¨","V":"√","W":"∑","X":"≈","Y":"¥","Z":"Ω","1":"¡","2":"™","3":"£","4":"¢","5":"∞","6":"§","7":"¶","8":"•","9":"ª","0":"º","`":"`","-":"–","=":"≠","[":"“","]":"‘","\\":"«","/":"÷",";":"…","'":"æ",",":"≤",".":"≥"},
+
+	encipher: function (pt) {
+		var ct = "";
+		for ( i = 0; i < pt.length; i++) {
+			var match = pt.charAt(i).toUpperCase();
+			ct += this.conversion_table[match];
+		}
+		console.log(ct);
+	},
+
+	decipher: function(ct) {
+		var pt = "";
+		for ( i = 0; i < ct.length; i++ ) {
+			for (key in this.conversion_table) {
+				if (this.conversion_table[key] === ct.charAt(i)) {
+					pt += key;
+				}
+			}
+		}
+		console.log(pt);
+	}
+};
